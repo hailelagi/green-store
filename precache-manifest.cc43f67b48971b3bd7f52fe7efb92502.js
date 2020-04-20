a72f450c8b29a271d6901ebe71a43a6491ba84ec
obsessedyouth <52631736+obsessedyouth@users.noreply.github.com>
2020-04-20 18:30:19 +0100

    build version one

---
+++ precache-manifest.cc43f67b48971b3bd7f52fe7efb92502.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95a0fdd8ef553665c19bb9e75a662eeb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "497d41a956c87fd86096",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "1471ea0e2801af153167",
+    "url": "/static/css/main.b70fa472.chunk.css"
+  },
+  {
+    "revision": "497d41a956c87fd86096",
+    "url": "/static/js/2.74fccd06.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.74fccd06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1471ea0e2801af153167",
+    "url": "/static/js/main.a624505f.chunk.js"
+  },
+  {
+    "revision": "2d4e496187394d4b08ce",
+    "url": "/static/js/runtime-main.16ebf261.js"
+  }
+]);
